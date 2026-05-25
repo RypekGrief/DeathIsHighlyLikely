5279aaf7a987c12f564e422559960d075eb5f502
RypekGrief <recepgul826@gmail.com>
2026-05-25 12:29:40 +0300

    Update MySubModule.cs
    Some unnecessary comment lines have been deleted.

--- DeathIsHighlyLikely/MySubModule.cs
+++ DeathIsHighlyLikely/MySubModule.cs
@@ -12,10 +12,8 @@ namespace DeathIsHighlyLikely
 
             if (game.GameType is Campaign)
             {
-                // 1. Senin kusursuz çalışan 3D savaş modelin
                 gameStarterObject.AddModel(new DeathIsHighlyLikelyModel());
 
-                // 2. Sadece harita simülasyonlarında çalışacak olan yeni modelimiz
                 gameStarterObject.AddModel(new MapSimulationDeathModel());
             }
         }
